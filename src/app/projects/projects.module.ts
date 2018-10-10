f363d563d18a015fb733f24b42c5b52736d2a12c
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-10 14:52:47 -0400

    lab09

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsContainerComponent } from './projects-container/projects-container.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProjectsRoutingModule
   ],
-  declarations: [ProjectsContainerComponent, ProjectListComponent]
+  declarations: [ProjectsContainerComponent, ProjectListComponent, ProjectCardComponent]
 })
 export class ProjectsModule { }
