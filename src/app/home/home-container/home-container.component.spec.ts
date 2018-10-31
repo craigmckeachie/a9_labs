31184679cd627f39d478d451662bb94b4505f559
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-31 12:53:36 -0400

    complete

--- src/app/home/home-container/home-container.component.spec.ts
+++ src/app/home/home-container/home-container.component.spec.ts
@@ -4,6 +4,7 @@ import { HomeContainerComponent } from './home-container.component';
 describe('HomeContainerComponent', () => {
   let component: HomeContainerComponent;
   let fixture: ComponentFixture<HomeContainerComponent>;
+  let h1: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,10 +15,23 @@ describe('HomeContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeContainerComponent);
     component = fixture.componentInstance;
+    h1 = fixture.debugElement.nativeElement.querySelector('h1');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title in h1 tag', () => {
+    expect(h1.textContent).toEqual('');
+  });
+
+  it('changing title, updates h1', () => {
+    const title = 'Home';
+    component.title = title;
+    expect(h1.textContent).not.toContain(title, 'before detectChanges');
+    fixture.detectChanges();
+    expect(h1.textContent).toContain(title);
   });
 });
--- src/app/home/home-container/home-container.component.ts
+++ src/app/home/home-container/home-container.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-container.component.css']
 })
 export class HomeContainerComponent implements OnInit {
+  title = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
