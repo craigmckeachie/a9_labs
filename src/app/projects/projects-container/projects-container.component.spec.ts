98b4157d3212fa85a157da77989a8967b8e897fd
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-31 14:26:23 -0400

    complete

--- src/app/projects/projects-container/projects-container.component.spec.ts
+++ src/app/projects/projects-container/projects-container.component.spec.ts
@@ -1,25 +1,48 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ProjectsContainerComponent } from './projects-container.component';
+import { ProjectsContainerComponent } from './projects-container.component';
+import { Component, Input } from '@angular/core';
+import { Project } from '../shared/project.model';
+import { Observable, of } from 'rxjs';
+import { PROJECTS } from '../shared/mock-projects';
+import { ProjectService } from '../shared/project.service';
 
-// describe('ProjectsContainerComponent', () => {
-//   let component: ProjectsContainerComponent;
-//   let fixture: ComponentFixture<ProjectsContainerComponent>;
+@Component({ selector: 'app-project-list', template: '' })
+class ProjectListStubComponent {
+  @Input()
+  projects: Project[] = [];
+}
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ProjectsContainerComponent ]
-//     })
-//     .compileComponents();
-//   }));
+export class ProjectServiceStub {
+  listByName(): Observable<Project[]> {
+    return of(PROJECTS);
+  }
+}
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ProjectsContainerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('ProjectsContainerComponent', () => {
+  let component: ProjectsContainerComponent;
+  let fixture: ComponentFixture<ProjectsContainerComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProjectsContainerComponent, ProjectListStubComponent],
+      providers: [{ provide: ProjectService, useClass: ProjectServiceStub }]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectsContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have projects', async(() => {
+    fixture.whenStable().then(() => {
+      expect(component.projects.length).toEqual(7);
+    });
+  }));
+});
