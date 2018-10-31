2da505d83bd435d71105bc54cd16a198e806f6ec
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-31 14:40:59 -0400

    complete

--- src/app/projects/shared/project.service.spec.ts
+++ src/app/projects/shared/project.service.spec.ts
@@ -1,12 +1,53 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { ProjectService } from './project.service';
+import { ProjectService } from './project.service';
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { PROJECTS } from './mock-projects';
 
-// describe('ProjectService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('ProjectService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let service: ProjectService;
+  let projectsUrl: string;
 
-//   it('should be created', () => {
-//     const service: ProjectService = TestBed.get(ProjectService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(ProjectService);
+    projectsUrl = environment.backendUrl + '/projects/';
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should list projects', () => {
+    service.list().subscribe(data => expect(data).toEqual(PROJECTS));
+    const request = httpTestingController.expectOne(projectsUrl);
+    request.flush(PROJECTS);
+  });
+
+  it('should return user friendly error when listing projects', () => {
+    const notFoundErrorResponse = { status: 404, statusText: 'Not Found' };
+    const content = 'The requested URL was not found on the server.';
+    service.list().subscribe(
+      data => {},
+      error => {
+        expect(error).toEqual('An error occurred loading the projects.');
+      }
+    );
+    const request = httpTestingController.expectOne(projectsUrl);
+    request.flush(content, notFoundErrorResponse);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+});
