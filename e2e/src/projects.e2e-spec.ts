2e0f2ea690c7e7f3cdc3387141fe57c6c98fafd7
Craig McKeachie <craigdmckeachie@gmail.com>
2018-12-12 14:43:33 -0500

    complete

--- e2e/src/projects.e2e-spec.ts
+++ e2e/src/projects.e2e-spec.ts
@@ -11,4 +11,22 @@ describe('Projects', () => {
     page.navigateTo();
     expect(page.projectNameHeaders.count()).toEqual(99);
   });
+
+  describe('when editing', () => {
+    beforeEach(() => {
+      page.navigateTo();
+      page.editButton.click();
+    });
+
+    it('should show form', () => {
+      expect(page.projectForm.isPresent()).toEqual(true);
+    });
+
+    it('should save updated project name', () => {
+      const updatedName = 'updated project name';
+      page.updateName(updatedName);
+      page.saveButton.click();
+      expect(page.firstProjectHeader.getText()).toEqual(updatedName);
+    });
+  });
 });
--- e2e/src/projects.po.ts
+++ e2e/src/projects.po.ts
@@ -1,8 +1,27 @@
-import { ElementArrayFinder, by, element, browser } from 'protractor';
+import {
+  ElementArrayFinder,
+  by,
+  element,
+  browser,
+  ElementFinder
+} from 'protractor';
 
 export class ProjectsPage {
   projectNameHeaders: ElementArrayFinder = element.all(by.css('h5.strong'));
+  editButton: ElementFinder = element(by.buttonText('Edit'));
+  saveButton: ElementFinder = element(by.buttonText('Save'));
+  projectForm: ElementFinder = element(by.tagName('form'));
+  projectNameInput: ElementFinder = element(by.name('name'));
+  firstProjectHeader: ElementFinder = this.projectNameHeaders.first();
+
   navigateTo() {
     return browser.get('/projects');
+  }
+
+  updateName(name: string) {
+    const input = this.projectNameInput;
+    input.clear().then(() => {
+      input.sendKeys(name);
+    });
   }
 }
